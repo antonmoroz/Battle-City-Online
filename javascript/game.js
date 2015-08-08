e87da4a9c08b8ab46499a8ce270a970de65ace37
Anton Moroz <antonmoroz@hotmail.com>
2015-08-09 00:10:36 +0300

    Added main game class

---
+++ javascript/game.js
@@ -0,0 +1,24 @@
+function BattleCity() {
+}
+
+/**
+ * Init game
+ * @param container {String} id of container
+ */
+BattleCity.prototype.init = function(container) {
+	//TODO: implement it
+};
+
+/**
+ * Run game
+ */
+BattleCity.prototype.run = function() {
+	//TODO: implement it
+};
+
+/**
+ * Render game
+ */
+BattleCity.prototype.render = function() {
+	//TODO: implement it
+};
