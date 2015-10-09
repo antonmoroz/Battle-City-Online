47c3c2cc655b3e802c51c2b6fe6b9493564ffaeb
Anton Moroz <antonmoroz@hotmail.com>
2015-10-10 00:41:53 +0300

    Daily commit

--- javascript/game.js
+++ javascript/game.js
@@ -1,24 +1,28 @@
-function BattleCity() {
-}
+define(function () {
+    function BattleCity() {
+    }
 
-/**
- * Init game
- * @param container {String} id of container
- */
-BattleCity.prototype.init = function(container) {
-	//TODO: implement it
-};
+    /**
+     * Init game
+     * @param container {String} id of container
+     */
+    BattleCity.prototype.init = function (container) {
+        //TODO: implement it
+    };
 
-/**
- * Run game
- */
-BattleCity.prototype.run = function() {
-	//TODO: implement it
-};
+    /**
+     * Run game
+     */
+    BattleCity.prototype.run = function () {
+        //TODO: implement it
+    };
 
-/**
- * Render game
- */
-BattleCity.prototype.render = function() {
-	//TODO: implement it
-};
+    /**
+     * Render game
+     */
+    BattleCity.prototype.render = function () {
+        //TODO: implement it
+    };
+
+    return BattleCity;
+});
--- javascript/main.js
+++ javascript/main.js
@@ -1,2 +1,10 @@
-function onBodyLoad() {
-}
+require.config({
+    paths: {
+        baseUrl: "javascript"
+    }
+});
+
+require(["game"], function (BattleCity) {
+    var game = new BattleCity();
+    game.run();
+});
